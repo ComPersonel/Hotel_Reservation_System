b0358e6214f605176b107d94b76e37965da6f2eb
ComPersonel <losdsneel@gmail.com>
2025-12-06 23:49:39 +0200

    Kommentointi 1

--- Hotel_Reservation_System/hotel_system.h
+++ Hotel_Reservation_System/hotel_system.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// Sisältää kaikki tarvittavat Include ja Namespace asiat headerissä, jotta cpp tiedostoissa on vähemmän clutter
+// Sisältää kaikki tarvittavat Include ja Namespace asiat, jotta cpp tiedostoissa on vähemmän roskaa
 # include <iostream>
 # include <string>
 # include <algorithm>
@@ -10,35 +10,60 @@
 # include <vector>
 # include <iomanip>
 # include <limits>
-# include <locale>
 
 using namespace std;
 
 // Room_data struct joka sisältää kaiken huone datan
+// floor		= Huoneen kerros + osa indexiä
+// number		= Huoneen numero + osa indexiä
+// reserved		= Huoneen tila
+// type			= Yhden vai Kahden hengen huone
+// tier			= Perus, Preemium vai Luksus huone
+// coupon		= Huoneeseen sidottu alennus
 struct room_data {
-	int floor = 0; // Huoneen kerros
-	int number = 0; // Huone numero
-	bool reserved = false; // Varattu?
-	std::string type = "single"; // Single, Double
-	std::string tier = "basic"; // Basic, Premium, Deluxe
-	int coupon = 0; // Kupongin alennus
+	int floor = 0;
+	int number = 0;
+	bool reserved = false;
+	std::string type = "single";
+	std::string tier = "basic";
+	int coupon = 0;
 };
 
 // Reservation_data struct joka sisältää kaiken varaus datan
+// index		= Varauksen indexi helpompaa löytämistä varten
+// reservation	= Varauksen numero
+// room_floor	= Varatun huoneen kerros
+// room_number	= Varatun huoneen numero
+// name			= Varaajan käyttäjänimi
+// stay			= Kuinka monta päivää huone on varattu
 struct reservation_data {
-	int index; // Varauksen index
-	int reservation; // Varaus numero
-	int room_floor; // Huoneen kerros
-	int room_number; // Huoneen numero
-	std::string name; // Käyttäjän nimi
-	int stay; // Keston pituus
+	int index;
+	int reservation;
+	int room_floor;
+	int room_number;
+	std::string name;
+	int stay;
 };
 
-// Main
+// Main.cpp functiot
+// menu			= Pää käyttö menu
+// dev_menu		= Dev menu
 void menu(vector<vector<room_data>>&, vector<reservation_data>&);
 void dev_menu(vector<vector<room_data>>&, vector<reservation_data>&);
 
-// File Manager funktiot
+// File Manager.cpp functiot
+// check_hotel				= Tarkistaa hotel_data.txt tiedoston olemassa olon
+// check_rooms				= Tarkistaa room_data.txt tiedoston olemassa olon
+// check_reservations		= Tarkistaa reservation_data.txt tiedoston olemassa olon
+// load_room_data			= Lataa room_data.txt tiedoston sisällön käytettävään muotoon
+// save_room_data			= Tallentaa muutetun huone datan room_data.txt tiedostoon
+// generate_room_data		= Luo uuden huone datan tarvittaessa
+// load_reservation_data	= Lataa reservation_data.txt tiedoston sisällön käytettävään muotoon
+// save_reservation_data	= Tallentaa muutetun varaus datan reservation_data.txt tiedostoon
+// empty_reservation_data	= Luo tai tyhjentää reservation_data.txt tiedoston tarvittaessa
+// define_hotel_size		= Lukee hotel_data.txt tiedostosta hotellin kerros ja huone määrän, palauttaa tämän informaation vectorina
+// create_hotel_size_random	= Luo satunnaisen hotelli informaation
+// create_hotel_size_input	= Luo hotellin koko informaation käyttäjän syötteestä
 void check_hotel();
 void check_rooms(vector<vector<room_data>>&);
 void check_reservations();
@@ -47,17 +72,39 @@ void save_room_data(vector<vector<room_data>>);
 void generate_room_data(vector<vector<room_data>>&);
 void load_reservation_data(vector<reservation_data>&, vector<vector<room_data>>&);
 void save_reservation_data(vector<reservation_data>);
-vector<int> define_hotel_size();
 void empty_reservation_data();
+vector<int> define_hotel_size();
 void create_hotel_size_random();
 void create_hotel_size_input( int, int);
 
-// Tasks funktiot
+// Tasks.cpp functiot
+// random_num					= Palauttaa satunnaisen arvon 2 annetun int arvon väliltä
+// input_int					= Ottaa syötteeksi int arvon 2 annetun int arvon väliltä. Palauttaa onnistuneen syötteen
+// input_name					= Ottaa syötteeksi tekstiä ja palauttaa sen ilman ; merkkejä teknisistä syöstä
+// yes_no						= Ottaa syötteenä Y tai N. Palauttaa bool arvon
+// enter						= Pyytää käyttäjää painamaan enter näppäintä jatkaakseen
+// type_desc					= Palauttaa kuvaavan tekstin huoneen tyypistä
+// tier_desc					= Palauttaa kuvaavan tekstin huoneen tasosta
+// print_room					= Tulostaa huoneen numeron, tyypin ja tason
+// print_reservation			= Tulostaa varauksen numeron, nimen ja keston
+// any_free_rooms				= Palauttaa onko hotellissa yhtään vapaata huonetta
+// free_rooms					= Palauttaa onko vapaita huoneita tietyltä kerrokselta, tyypiltä tai tasolta
+// output_room_number			= Palauttaa huoneen numeron käyttäen sen indexi arvoja
+// index_from_number			= Palauttaa huoneen indexit vectorina sen numerosta
+// create_reservation			= Luo varauksen annetuista tiedoista
+// create_reservation_number	= Luo uniikin varaus numeron 10000-99999 väliltä
+// collect_rooms				= Kerää tietynlaisia huoneita vector taulukkoon
+// random_from_list				= Valitsee satunnaisen vaihtoehdon vector taulukosta
+// print_bill					= Tulostaa laskun
+// print_cost					= Laskee varauksen kokonais hinnan
+// find_by_name					= Etsii varauksen nimellä
+// find_by_reservation			= Etsii varauksen sen numerolla
+// clear_reservations			= Poistaa varauksen
 int random_num(int, int);
 int input_int(int, int);
+std::string input_name();
 bool yes_no();
 void enter();
-std::string input_name();
 std::string type_desc(room_data);
 std::string tier_desc(room_data);
 void print_room(room_data);
@@ -76,12 +123,22 @@ vector<reservation_data> find_by_name(vector<reservation_data>, std::string);
 vector<reservation_data> find_by_reservation(vector<reservation_data>, int);
 void clear_reservations(vector<vector<room_data>>&, vector<reservation_data>&);
 
-// Komento funktiot
+// Commands.cpp function | nimetty suomeksi, koska ne on suoraan sidottu käyttäjän komentoihin
+// varaa_huone				= Kerää informaation tehdäkseen varauksen
+// tulosta_varaus			= Tulostaa viimeisimmän varauksen
+// tulosta_kaikki_varaukset	= Tulostaa kaikki varaukset
+// etsi_varaus				= Etsii varauksen joko nimellä tai varausnumerolla
+// muokkaa_varaus			= Muokkaa varausta
+// peru_varaus				= Peruuta varaus (muokkaa_varaus aliohjelma)
+// muokkaa_nimi				= Muokkaa varaajan nimeä (muokkaa_varaus aliohjelma)
+// muokkaa_kesto			= Muokkaa varauksen kestoa (muokkaa_varaus aliohjelma)
+// muokkaa_huone			= Muokkaa varattua huonetta (muokkaa_varaus aliohjelma)
+// uusi_hotelli				= Luo kokonaan uuden hotellin
 void varaa_huone(vector<vector<room_data>>&, vector<reservation_data>&);
 void tulosta_varaus(vector<vector<room_data>>, vector<reservation_data>);
+void tulosta_kaikki_varaukset(vector<vector<room_data>>, vector<reservation_data>);
 void etsi_varaus(vector<vector<room_data>>, vector<reservation_data>);
 void muokkaa_varaus(vector<vector<room_data>>&, vector<reservation_data>&);
-void tulosta_kaikki_varaukset(vector<vector<room_data>>, vector<reservation_data>);
 int peru_varaus(vector<vector<room_data>>&, vector<reservation_data>&, reservation_data);
 void muokkaa_nimi(vector<reservation_data>&, reservation_data);
 void muokkaa_kesto(vector<reservation_data>&, reservation_data);
--- Hotel_Reservation_System/main.cpp
+++ Hotel_Reservation_System/main.cpp
@@ -5,32 +5,48 @@
 
 using namespace std;
 
-// Main Funktio ----------------------------------------------------
+// Main Function ------------------------------------------------------------------------------------
+// Luo pohja datan ja tarkastaa tiedostojen tilan
+// Ajaa Menu function, joka suorittaa loput
+// Tallentaa uudet tiedot ja sulkee
+//
 int main() {
 
-	srand(time(0)); // Generoi random siemen
+	// Luo satunnaisten numeroiden siemenen
+	// Asettaa couttiin kahden desimaalin tuloste
+	// Asettaa Suomalaisen localen
+	srand(time(0));
 	cout << fixed << setprecision(2);
-
 	setlocale(LC_ALL, "FI_fi");
 
-	// Alusta muuttuujat
+	// Tarkistaa onko hotel_data.txt tiedost kunnossa
+	// Jos ei luo uuden tiedoston, jotta ohjelma voidaan ajaa ilman ongelmia
 	check_hotel();
 
+	// Tallentaa hotellin kerros ja huone määrä vectoriin, jotta sitä voidaan käyttää muussa ohjelmassa
+	// Luodaan 2d vectori taulukkoon muotoa[kerros][numero] käyttäen hotellin kokoa pohjana
+	// Luodaan vectori taulukko johon säilötään varaukset
 	vector<int> hotel = define_hotel_size();
-
 	vector<vector<room_data>> rooms_array(hotel[0], vector<room_data>(hotel[1]));
 	vector<reservation_data> reservations_array;
 
+	// Tarkistetaan onko room_data.txt tiedosto olemassa ja jos ei ole luodaan uusi
+	// Tarkistetaan onko reservation_data.txt olemassa ja varoitetaan puuttuvista varauksista
 	check_rooms(rooms_array);
 	check_reservations();
 
+	// Ladataan huone data "rooms_array" taulukkoon, jotta sitä voidaan käyttää
+	// Ladataan varaukset "reservations_array" taulukkoon, jotta sitä voidaan käyttää
 	load_room_data(rooms_array);
 	load_reservation_data(reservations_array, rooms_array);
 
+	// Siirrytään menu ohjelmaan, jotta käyttäjä voi käyttää ohjelmaa
 	menu(rooms_array, reservations_array);
 
+	// Kun menu suljetaan tallenetaan päivitetty huone data room_data.txt tiedostoon
 	save_room_data(rooms_array);
 
+	// Jos varauksia on tallenetaan ne, mutta muuten tyhjennetään tiedosto, jotta vanhat varaukset eivät jää lojumaan
 	if (!reservations_array.empty()) {
 		save_reservation_data(reservations_array);
 	}
@@ -41,15 +57,20 @@ int main() {
 	return 0;
 }
 
-// Valikko ---------------------------------------------------------
+// Valikko --------------------------------------------------------------------------------------
+// Avataan valikko josta käyttäjä voi siirtyä eri toimintoihin
+// 
 void menu(vector<vector<room_data>>& rooms_array, vector<reservation_data>& reservations_array) {
 
-	// Alusta muuttuujat
+	// Alusta command muuttuja, ojonh käyttäjä laittaa toivotun tehtävän
 	int command;
 
-	do { // Pyydä tehtävä Numeroa
+	// käyttäen do-while looppia pysytään menussa kunnes käyttäjä tahtoo sulkea ohjelman
+	// syöttämällä 0 ohjelma poistuu loopista ja palaa main ohjelmaan jossa se voidaan sulkea
+	do {
 		system("cls");
 
+		// Tulostetaan tehtävä vaihtoehdot
 		cout << "--------------------------" << endl;
 		cout << left << setw(25) << "Tehtävät : " << "|" << endl;
 		cout << left << setw(25) << "[1] : Varaa Huone" << "|" << endl;
@@ -61,12 +82,15 @@ void menu(vector<vector<room_data>>& rooms_array, vector<reservation_data>& rese
 		cout << "--------------------------" << endl;
 		cout << "Syötä toiminto >> ";
 
+		// Syötteen tarkistus, joka tarkistaa onko syöte onnistunut
+		// Jos syöte ei onnistu tyhjennetään se ja asetetaan command = -1 jotta voimme jatkaa loopissa
 		if (!(cin >> command)) {
 			cin.clear();
 			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 			command = -1;
 		}
 
+		// Switch-case joka tarkastaa syötetyn arvon ja siirtyy tehtävään jos sitä vastaava arvo on syötetty
 		switch (command) {
 		case 1: varaa_huone(rooms_array, reservations_array);  break;
 		case 2: tulosta_varaus(rooms_array, reservations_array);  break;
@@ -74,8 +98,10 @@ void menu(vector<vector<room_data>>& rooms_array, vector<reservation_data>& rese
 		case 4: muokkaa_varaus(rooms_array, reservations_array);  break;
 		case 5: tulosta_kaikki_varaukset(rooms_array, reservations_array);  break;
 
+		// 0 Erikois tila jossa vain tulostetaan viesti ja poistutaan ohjelmasta
 		case 0: cout << "Suljetaan Ohjelma";  break;
 
+		// 420 Erikois tila jossa avataan Dev menu
 		case 420: dev_menu(rooms_array, reservations_array);  break;
 
 		default: cout << "Virhetilanne";
@@ -86,16 +112,21 @@ void menu(vector<vector<room_data>>& rooms_array, vector<reservation_data>& rese
 }
 
 
-// Dev Valikko ---------------------------------------------------------
+// Dev Valikko -------------------------------------------------------------------------------
+// Avataan Dev valikko jossa käyttäjä voi vaikuttaa tiedostojen sisältöön, mutta voi aiheuttaa ongelmia
+//
 void dev_menu(vector<vector<room_data>>& rooms_array, vector<reservation_data>& reservations_array) {
 
-	// Alusta muuttuujat
+	// Alusta command muuttuja, ojonh käyttäjä laittaa toivotun tehtävän
 	int command;
 
-	do { // Pyydä tehtävä Numeroa
+	// käyttäen do-while looppia pysytään menussa kunnes käyttäjä tahtoo sulkea ohjelman
+	// syöttämällä 0 ohjelma poistuu loopista ja palaa main ohjelmaan jossa se voidaan sulkea
+	do {
 
 		system("cls");
 
+		// Tulostetaan tehtävä vaihtoehdot
 		cout << "--------------------------" << endl;
 		cout << left << setw(25) << "Developer Menu" << "|" << endl;
 		cout << left << setw(25) << "[1] : Luo Huoonedata" << "|" << endl;
@@ -106,17 +137,21 @@ void dev_menu(vector<vector<room_data>>& rooms_array, vector<reservation_data>& 
 		cout << "OSA KOMENNOISTA POISTAA DATAA TAI VAATII OHJELMAN UUDELLEEN KÄYNISTYKSEN" << endl;
 		cout << "Syötä toiminto >> ";
 
+		// Syötteen tarkistus, joka tarkistaa onko syöte onnistunut
+		// Jos syöte ei onnistu tyhjennetään se ja asetetaan command = -1 jotta voimme jatkaa loopissa
 		if (!(cin >> command)) {
 			cin.clear();
 			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 			command = -1;
 		}
 
+		// Switch-case joka tarkastaa syötetyn arvon ja siirtyy tehtävään jos sitä vastaava arvo on syötetty
 		switch (command) {
 		case 1: generate_room_data(rooms_array);  break;
 		case 2: clear_reservations(rooms_array, reservations_array);  break;
 		case 3: uusi_hotelli();  break;
 
+		// 0 Erikois tila jossa vain tulostetaan viesti ja poistutaan ohjelmasta
 		case 0: cout << "Suljetaan Ohjelma";  break;
 
 		default: cout << "Virhetilanne";
